3a19ade0b062dd545d3ebacdec310ed5c46f98bc
Frank Collins <fwc3rd@gmail.com>
2023-03-02 23:26:33 -0500

    Update DataCall.ts

--- utility/DataCall.ts
+++ utility/DataCall.ts
@@ -1,79 +1,79 @@
-import $ from 'jquery'
-import Axios from 'axios'
+// import $ from 'jquery'
+// import Axios from 'axios'
 
-// export default async function DataCall ( method:string, url:string, data:(string|null)) { 
-//     const ajaxCall = async () => {
-//         if (method === 'ajax') {
-//             let ajax = $.ajax({
-//                 method: 'get',
-//                 url: url,
-//                 data: 'json',
-//                 success: (ajaxdata) => {
-//                     console.log('ajaxdata')
-//                     console.log(ajaxdata)
-//                     return ajaxdata
-//                 }
-//             })
-//             return ajax
-//         }
-//     }
-//         return ajaxCall()
+// // export default async function DataCall ( method:string, url:string, data:(string|null)) { 
+// //     const ajaxCall = async () => {
+// //         if (method === 'ajax') {
+// //             let ajax = $.ajax({
+// //                 method: 'get',
+// //                 url: url,
+// //                 data: 'json',
+// //                 success: (ajaxdata) => {
+// //                     console.log('ajaxdata')
+// //                     console.log(ajaxdata)
+// //                     return ajaxdata
+// //                 }
+// //             })
+// //             return ajax
+// //         }
+// //     }
+// //         return ajaxCall()
 
-//         if (method === 'axios') {
+// //         if (method === 'axios') {
 
-//         }    
+// //         }    
 
-//         }
+// //         }
 
-export default async function DataCall (method:string, url:string, data:(string|null)) {
-    console.log('url')
-    console.log(url)
+// export default async function DataCall (method:string, url:string, data:(string|null)) {
+//     console.log('url')
+//     console.log(url)
 
-    class Call {
+//     class Call {
 
-        constructor(url:any) {
+//         constructor(url:any) {
             
-            this.url = url
-        }      
-    // method getters
-    get ajax() {
-        console.log("we are over here")
-        return this.ajaxcall()
-    }
+//             this.url = url
+//         }      
+//     // method getters
+//     get ajax() {
+//         console.log("we are over here")
+//         return this.ajaxcall()
+//     }
 
-    // methods 
-    ajaxcall() {
-            const ajaxCall = () => {
-                $.ajax({
-                // let ajax = $.ajax({
-                    method: 'get',
-                    url: url,
-                    data: 'json',
-                    // success: (ajaxdata) => {
-                    //     console.log('ajaxdata')
-                    //     console.log(ajaxdata)
-                    //     return ajaxdata
-                }).then( (data) => {
-                    console.log('data')
-                    console.log(data)
-                    return data            
-                })            
-            }
-            return ajaxCall()
-    }
-}   // ajax call ending 
+//     // methods 
+//     ajaxcall() {
+//             const ajaxCall = () => {
+//                 $.ajax({
+//                 // let ajax = $.ajax({
+//                     method: 'get',
+//                     url: url,
+//                     data: 'json',
+//                     // success: (ajaxdata) => {
+//                     //     console.log('ajaxdata')
+//                     //     console.log(ajaxdata)
+//                     //     return ajaxdata
+//                 }).then( (data) => {
+//                     console.log('data')
+//                     console.log(data)
+//                     return data            
+//                 })            
+//             }
+//             return ajaxCall()
+//     }
+// }   // ajax call ending 
  
-        if (method === 'ajax') {
-            console.log("we are over here doing it this way")
-            const returnajaxcall = new Call().ajax
-            return returnajaxcall
-            // console.log(returnajaxcall.responseJSON)
-            // let actualdata = returnajaxcall.responseJSON.getdata
-            // console.log('actualdata')
-            // console.log(actualdata)
-        }
+//         if (method === 'ajax') {
+//             console.log("we are over here doing it this way")
+//             const returnajaxcall = new Call().ajax
+//             return returnajaxcall
+//             // console.log(returnajaxcall.responseJSON)
+//             // let actualdata = returnajaxcall.responseJSON.getdata
+//             // console.log('actualdata')
+//             // console.log(actualdata)
+//         }
 
 
 
 
-}       // function end
+// }       // function end
