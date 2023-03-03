e23794e6995e9ec646756931635f7ff913785d53
Frank Collins <fwc3rd@gmail.com>
2023-03-02 23:17:55 -0500

    Add files via upload

---
+++ components/FamilyTreeContainer2.tsx
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container'
+
+export default function Family2 () {
+    return (
+        <Container
+        
+        >
+            <h1 style={{ color: 'white'}}> hey nice one </h1>
+        </Container>
+    )
+}
