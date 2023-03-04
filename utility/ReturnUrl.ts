a592816882fd49e26d0dfc141a97f1e898becc26
Frank Collins <fwc3rd@gmail.com>
2023-03-04 17:52:08 -0500

    Create ReturnUrl.ts

--- utility/ReturnUrl.ts
+++ utility/ReturnUrl.ts
@@ -3,7 +3,8 @@ async function ReturnUrl (context:any) {
     if (process.env.NODE_ENV === "production") {
       // * this has to be set to the production site url. if you want to download this app and use in build mode locally then you'd have to switch this to localhost:3000
 
-        return 'http://localhost:3000'
+        // return 'http://localhost:3000'
+        return 'http://test-8vsej4rim-frankcollins3.vercel.app'
         // return 'http://test-coral-two.vercel.app'
         // return `https://${context.req.rawHeaders[1]}`;
       } 
