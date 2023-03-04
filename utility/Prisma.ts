4fe4c154b43be782d7abb080d662aced5996edcc
Frank Collins <fwc3rd@gmail.com>
2023-03-04 17:36:04 -0500

    Delete utility directory

--- utility/Prisma.ts
+++
@@ -1,7 +0,0 @@
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient()
-
-export default async function Client () {
-    console.log("were just importing a bit right now");
-
-}
