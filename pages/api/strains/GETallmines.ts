9b9f7c2ac81fb4f6b78a9a159af25039c9e04113
Frank Collins <fwc3rd@gmail.com>
2023-03-04 22:25:05 -0500

    Delete pages directory

--- pages/api/strains/GETallmines.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, PrismaClient } from '@prisma/client';
-let prisma = new PrismaClient()
-
-export default async function GETallmines (req, res) {
-        let allmines = await prisma.mines.findMany() || 'no mines yet';
-        res.json( { allmines: allmines })
-}
