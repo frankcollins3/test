93b81596d4f951b9a3839e0fb258583f6962a42b
Frank Collins <fwc3rd@gmail.com>
2023-03-04 20:03:49 -0500

    testing

--- utility/ReturnUrl.ts
+++ utility/ReturnUrl.ts
@@ -4,7 +4,7 @@ async function ReturnUrl (context) {
       // * this has to be set to the production site url. if you want to download this app and use in build mode locally then you'd have to switch this to localhost:3000
 
       // return `https://${context.req.rawHeaders[1]}`;
-      let httpsvar = context.slice(0, context.length - 9)
+      let httpsvar = context.slice(0, context.length - 15)
       let httpvar = httpsvar(4)
 
       console.log('httpsvar')
