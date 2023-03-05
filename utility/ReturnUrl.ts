30bb89367ecf10c21e551145c79623b0a43aea08
Frank Collins <fwc3rd@gmail.com>
2023-03-04 20:17:46 -0500

    Update ReturnUrl.ts

--- utility/ReturnUrl.ts
+++ utility/ReturnUrl.ts
@@ -4,16 +4,22 @@ async function ReturnUrl (context) {
       // * this has to be set to the production site url. if you want to download this app and use in build mode locally then you'd have to switch this to localhost:3000
 
       // return `https://${context.req.rawHeaders[1]}`;
-      let httpsvar = context.slice(0, context.length - 9)
-      let httpvar = httpsvar(4)
 
-      console.log('httpsvar')
-      console.log(httpsvar)
+      // let httpsvar = context.slice(0, context.length - 9)
+      // let httpvar = httpsvar.slice(4)
 
-      console.log('httpvar')
-      console.log(httpvar)
+      // console.log('httpsvar')
+      // console.log(httpsvar)
 
-        return httpvar
+      // console.log('httpvar')
+      // console.log(httpvar)
+      console.log('context')
+      console.log(context)
+
+        return context;
+        // return "http://localhost:3000";
+
+        // return httpvar
         // return httpvar
         // console.log('window')
         // console.log(window)
