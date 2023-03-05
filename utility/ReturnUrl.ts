8b7c5e2ed1d0f4d69fd67318802247e37cf989a1
Frank Collins <fwc3rd@gmail.com>
2023-03-04 19:52:29 -0500

    ReturnURL [7:52pm]

--- utility/ReturnUrl.ts
+++ utility/ReturnUrl.ts
@@ -1,14 +1,25 @@
-async function ReturnUrl (context:any) {    
+async function ReturnUrl (context) {    
 
     if (process.env.NODE_ENV === "production") {
       // * this has to be set to the production site url. if you want to download this app and use in build mode locally then you'd have to switch this to localhost:3000
 
-      
-        return 'http://minenugget-8s7i033h5-frankcollins3.vercel.app/'
+      // return `https://${context.req.rawHeaders[1]}`;
+        return context
+        // console.log('window')
+        // console.log(window)
+        // console.log(window.location)
+
+        
+
+        return "http://localhost:3000";
+        // return `http://${context.req.rawHeaders[1]}`;
+        // 
         // return process.cwd()
-        // return 'http://test-8vsej4rim-frankcollins3.vercel.app'
-        // return 'http://test-coral-two.vercel.app'
-        // return `https://${context.req.rawHeaders[1]}`;
+
+        // if (process.env.NODE_ENV === "production") {
+          // return `https://${context.req.rawHeaders[1]}`;
+        // } else if (process.env.NODE_ENV !== "production") {
+        // }
       } 
       else  { 
             // this gets rid of the error
